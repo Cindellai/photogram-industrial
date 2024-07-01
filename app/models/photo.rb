5873d43a1d4a838f1580d580cc3c58e236f7c2b1
cindellai <cindellai26@gmail.com>
2024-07-01 15:33:58 -0500

    added scope for photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -23,4 +23,7 @@ class Photo < ApplicationRecord
   belongs_to :owner, class_name: "User", counter_cache: true
   validates :caption, presence: true
   validates :image, presence: true
+
+  scope :past_week, -> { where(created_at: 1.week.ago...) }
+  scope :by_likes, -> { order(likes_count: :desc) }
 end
